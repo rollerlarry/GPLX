ec3e9053bb1978ad0d8a15ddf544a2f64a474d0d
Trần Quốc Dũng <tranquocdung10081997@gmail.com>
2018-12-03 11:08:12 +0700

    cập nhật chức năng thi ngẫu nhiên
    lấy câu hỏi ra ngẫu nhiên
    word 2 câu
    excel 2 câu
    pp 1 câu

--- app/src/main/java/com/example/itcode/gplx/DAO/QuestionDAO.java
+++ app/src/main/java/com/example/itcode/gplx/DAO/QuestionDAO.java
@@ -8,6 +8,8 @@ import com.example.itcode.gplx.DTO.Question;
 import com.example.itcode.gplx.Helper.DBHelper;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 
 public class QuestionDAO {
     private static DBHelper dbHelper;
@@ -27,25 +29,62 @@ public class QuestionDAO {
             type[0]=0;
             type[1]=1;
             type[2]=2;
-            sqlSelect ="SELECT * FROM '"+TB_TABLE +"'" +
-                    "WHERE ID_LoaiCauHoi= '" +type[0] +"' OR ID_LoaiCauHoi= '" +type[1] +"' OR ID_LoaiCauHoi= '" +type[2]+"'"+
-                    "ORDER BY RANDOM() LIMIT 5";
+            Cursor cursor;
+            String sqlSelect0 = "SELECT * FROM '"+TB_TABLE +"'" +
+                    "WHERE ID_LoaiCauHoi= '" +type[0] +"' ORDER BY RANDOM() LIMIT 2";
+            cursor = sqLiteDatabase.rawQuery(sqlSelect0, null);
+            if (cursor.moveToFirst()){
+                do {
+                    Question question = new Question(cursor.getInt(0), cursor.getInt(1), cursor.getString(2),
+                            cursor.getString(3), cursor.getString(4), cursor.getString(5), cursor.getString(6),
+                            cursor.getString(7), cursor.getString(8), null);
+
+                    questionArrayList.add(question);
+                }while (cursor.moveToNext());
+            }
+            String sqlSelect1 = "SELECT * FROM '"+TB_TABLE +"'" +
+                    "WHERE ID_LoaiCauHoi= '" +type[1] +"' ORDER BY RANDOM() LIMIT 2";
+            cursor = sqLiteDatabase.rawQuery(sqlSelect1, null);
+            if (cursor.moveToFirst()){
+                do {
+                    Question question = new Question(cursor.getInt(0), cursor.getInt(1), cursor.getString(2),
+                            cursor.getString(3), cursor.getString(4), cursor.getString(5), cursor.getString(6),
+                            cursor.getString(7), cursor.getString(8), null);
+
+                    questionArrayList.add(question);
+                }while (cursor.moveToNext());
+            }
+            String sqlSelect2 = "SELECT * FROM '"+TB_TABLE +"'" +
+                    "WHERE ID_LoaiCauHoi= '" +type[2] +"' ORDER BY RANDOM() LIMIT 1";
+            cursor = sqLiteDatabase.rawQuery(sqlSelect2, null);
+            if (cursor.moveToFirst()){
+                do {
+                    Question question = new Question(cursor.getInt(0), cursor.getInt(1), cursor.getString(2),
+                            cursor.getString(3), cursor.getString(4), cursor.getString(5), cursor.getString(6),
+                            cursor.getString(7), cursor.getString(8), null);
+
+                    questionArrayList.add(question);
+                }while (cursor.moveToNext());
+            }
+//            sqlSelect ="SELECT * FROM '"+TB_TABLE +"'" +
+//                    "WHERE ID_LoaiCauHoi= '" +type[0] +"' OR ID_LoaiCauHoi= '" +type[1] +"' OR ID_LoaiCauHoi= '" +type[2]+"'"+
+//                    "ORDER BY RANDOM() LIMIT 5";
         }
         else
         {
             sqlSelect = "SELECT * FROM '"+TB_TABLE + "'WHERE ID_LoaiCauHoi = '" +typeExam + "'LIMIT 5";
+            Cursor cursor = sqLiteDatabase.rawQuery(sqlSelect, null);
+            if (cursor.moveToFirst()){
+                do {
+                    Question question = new Question(cursor.getInt(0), cursor.getInt(1), cursor.getString(2),
+                            cursor.getString(3), cursor.getString(4), cursor.getString(5), cursor.getString(6),
+                            cursor.getString(7), cursor.getString(8), null);
 
+                    questionArrayList.add(question);
+                }while (cursor.moveToNext());
+            }
         }
-        Cursor cursor = sqLiteDatabase.rawQuery(sqlSelect, null);
-        if (cursor.moveToFirst()){
-            do {
-                Question question = new Question(cursor.getInt(0), cursor.getInt(1), cursor.getString(2),
-                        cursor.getString(3), cursor.getString(4), cursor.getString(5), cursor.getString(6),
-                        cursor.getString(7), cursor.getString(8), null);
-
-                questionArrayList.add(question);
-            }while (cursor.moveToNext());
-        }
+        Collections.shuffle(questionArrayList,new Random());
         sqLiteDatabase.close();
         return questionArrayList;
     }
