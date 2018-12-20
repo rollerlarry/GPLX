86ce5164c5297c3114ae38d08afea00847d25efa
Roller Larry <rollerlarry51197@gmail.com>
2018-12-21 03:39:41 +0700

    cập nhật và sửa đổi vài lỗi nhỏ chức năng lưu câu hỏi

--- app/src/main/java/com/example/itcode/gplx/Activity/SaveQuestionActivity.java
+++ app/src/main/java/com/example/itcode/gplx/Activity/SaveQuestionActivity.java
@@ -1,16 +1,22 @@
 package com.example.itcode.gplx.Activity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.example.itcode.gplx.Adapter.SaveQuestionAdapter;
 import com.example.itcode.gplx.Controller.QuestionControler;
 import com.example.itcode.gplx.DTO.Question;
 import com.example.itcode.gplx.R;
+import com.example.itcode.gplx.Slide.ScreenSlideForExamActivity;
 
 import java.util.ArrayList;
 
@@ -18,28 +24,33 @@ public class SaveQuestionActivity extends AppCompatActivity {
     private ListView lvSaveQuestion;
     private ArrayList<Question> questionArrayList;
     private SaveQuestionAdapter saveQuestionAdapter;
+    private AdapterView.AdapterContextMenuInfo menuItem;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_save_question);
         lvSaveQuestion = findViewById(R.id.lvSaveQuestion);
+        //Lay danh sach cau hoi luu tu trong CSDL
         QuestionControler questionControler = new QuestionControler(this);
         questionArrayList = questionControler.getSaveQuestionList();
+
         saveQuestionAdapter = new SaveQuestionAdapter(this, R.layout.item_save_question, questionArrayList);
         lvSaveQuestion.setAdapter(saveQuestionAdapter);
+
         registerForContextMenu(lvSaveQuestion);
     }
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
-        getMenuInflater().inflate(R.menu.menu_context, menu);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_context, menu);
     }
 
     @Override
     public boolean onContextItemSelected(MenuItem item) {
-        System.out.println("!23333");
-        return super.onContextItemSelected(item);
+        menuItem = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        return true;
     }
 
     @Override
@@ -47,4 +58,6 @@ public class SaveQuestionActivity extends AppCompatActivity {
         super.onBackPressed();
         overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
     }
+
+
 }
--- app/src/main/java/com/example/itcode/gplx/Adapter/SaveQuestionAdapter.java
+++ app/src/main/java/com/example/itcode/gplx/Adapter/SaveQuestionAdapter.java
@@ -1,12 +1,16 @@
 package com.example.itcode.gplx.Adapter;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.itcode.gplx.Controller.QuestionControler;
 import com.example.itcode.gplx.DTO.Question;
@@ -14,7 +18,7 @@ import com.example.itcode.gplx.R;
 
 import java.util.ArrayList;
 
-public class SaveQuestionAdapter extends ArrayAdapter<Question> {
+public class SaveQuestionAdapter extends ArrayAdapter<Question>{
     private Context context;
     private int resource;
     private ArrayList<Question> questionArrayList;
@@ -28,7 +32,7 @@ public class SaveQuestionAdapter extends ArrayAdapter<Question> {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup viewGroup) {
+    public View getView(final int position, View convertView, ViewGroup viewGroup) {
         ViewHolder viewHolder;
         if (convertView == null){
             convertView = LayoutInflater.from(context).inflate(R.layout.item_save_question, viewGroup, false);
@@ -37,12 +41,16 @@ public class SaveQuestionAdapter extends ArrayAdapter<Question> {
             viewHolder.tvQuestion = convertView.findViewById(R.id.tvQuestion);
             viewHolder.tvType = convertView.findViewById(R.id.tvType);
             viewHolder.tvAnswerTrue = convertView.findViewById(R.id.tvAnswerTrue);
+            viewHolder.cardView = convertView.findViewById(R.id.cardView);
             convertView.setTag(viewHolder);
         } else {
             viewHolder = (ViewHolder) convertView.getTag();
         }
+        //Lay vi tri cau trong cai mang
         question = questionArrayList.get(position);
 
+
+        //Kiem tra loai cau hoi
         if (question.getQuestionTypeID() == 0){
             viewHolder.imvType.setImageDrawable(getContext().getDrawable(R.drawable.word));
             viewHolder.tvType.setText("Word");
@@ -54,6 +62,14 @@ public class SaveQuestionAdapter extends ArrayAdapter<Question> {
             viewHolder.tvType.setText("PowerPoint");
         }
 
+        //Su kien xoa cau hoi luu
+        viewHolder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertMess("Bạn có muốn xóa câu hỏi lưu này không ?", position);
+            }
+        });
+
         viewHolder.tvQuestion.setText(question.getTextQuestion());
         viewHolder.tvAnswerTrue.setText("ĐÁP ÁN : " + getCheckAnswer(question.getAnswerTrue()));
 
@@ -63,6 +79,35 @@ public class SaveQuestionAdapter extends ArrayAdapter<Question> {
     private static class ViewHolder{
         private ImageView imvType;
         private TextView tvType, tvQuestion, tvAnswerTrue;
+        private CardView cardView;
+    }
+
+    public void alertMess(String mess, final int position){
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setIcon(R.drawable.bell);
+        builder.setTitle("Thông báo");
+        builder.setMessage(mess);
+        builder.setPositiveButton("Có", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //Update cac hoi luu -> 0 -> xoa khoi ds luu
+                QuestionControler questionControler = new QuestionControler(getContext());
+                questionControler.deleteSaveQuestion(String.valueOf(questionArrayList.get(position).getQuestionID()));
+                //Xoa item khoi listview
+                questionArrayList.remove(position);
+                //Cap nhat lai listview
+                notifyDataSetChanged();
+                //Hien thong bao xoa thanh cong
+                Toast.makeText(getContext(), "Xóa thành công", Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNegativeButton("Không", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        builder.show();
     }
 
     //get answer true
